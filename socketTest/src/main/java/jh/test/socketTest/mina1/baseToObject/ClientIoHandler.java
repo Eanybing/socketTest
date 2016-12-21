482549d9ea105e420bf3cfb6a893c418474ca554
jhys <tonghuajianghan@gmail.com>
2016-12-21 13:40:31 +0800

    mina1 base 测试完成 简单的连接 发送string,基本类型可以了
    Change-Id: Ifdb1b8e9c8a74fe252727a14521e2e5df03ae570
    Signed-off-by: jhys <tonghuajianghan@gmail.com>

---
+++ socketTest/src/main/java/jh/test/socketTest/mina1/baseToObject/ClientIoHandler.java
@@ -0,0 +1,76 @@
+package jh.test.socketTest.mina1.baseToObject;
+
+import org.apache.mina.common.IdleStatus;
+import org.apache.mina.common.IoHandlerAdapter;
+import org.apache.mina.common.IoSession;
+
+public class ClientIoHandler extends IoHandlerAdapter{
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#sessionCreated(org.apache.mina.common.IoSession)
+	 */
+	@Override
+	public void sessionCreated(IoSession session) throws Exception {
+		// TODO Auto-generated method stub
+		super.sessionCreated(session);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#sessionOpened(org.apache.mina.common.IoSession)
+	 */
+	@Override
+	public void sessionOpened(IoSession session) throws Exception {
+		// TODO Auto-generated method stub
+		super.sessionOpened(session);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#sessionClosed(org.apache.mina.common.IoSession)
+	 */
+	@Override
+	public void sessionClosed(IoSession session) throws Exception {
+		// TODO Auto-generated method stub
+		super.sessionClosed(session);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#sessionIdle(org.apache.mina.common.IoSession, org.apache.mina.common.IdleStatus)
+	 */
+	@Override
+	public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+		// TODO Auto-generated method stub
+		super.sessionIdle(session, status);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#exceptionCaught(org.apache.mina.common.IoSession, java.lang.Throwable)
+	 */
+	@Override
+	public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+		// TODO Auto-generated method stub
+		super.exceptionCaught(session, cause);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#messageReceived(org.apache.mina.common.IoSession, java.lang.Object)
+	 */
+	@Override
+	public void messageReceived(IoSession session, Object message) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("--------client 接收消息 ---------------------");
+		String msg = message.toString();
+		System.out.println(msg);
+		
+		super.messageReceived(session, message);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.mina.common.IoHandlerAdapter#messageSent(org.apache.mina.common.IoSession, java.lang.Object)
+	 */
+	@Override
+	public void messageSent(IoSession session, Object message) throws Exception {
+		// TODO Auto-generated method stub
+		super.messageSent(session, message);
+	}
+	
+}
