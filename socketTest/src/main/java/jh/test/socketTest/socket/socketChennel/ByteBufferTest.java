59d8e03aa05923bff6932cc917f52797b3f610c5
jhys <tonghuajianghan@gmail.com>
2016-12-27 10:49:13 +0800

    socket

---
+++ socketTest/src/main/java/jh/test/socketTest/socket/socketChennel/ByteBufferTest.java
@@ -0,0 +1,21 @@
+package jh.test.socketTest.socket.socketChennel;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
+public class ByteBufferTest {
+	public static void main(String[] args) throws IOException {
+		
+		String str = "213425fgdgsd";
+		// string to byteBuffer
+		ByteBuffer bb = ByteBuffer.wrap(str.getBytes());
+		// byteBuffer to string
+		CharBuffer charBuffer = Charset.forName("utf-8").newDecoder().decode(bb.asReadOnlyBuffer());
+		String cc = charBuffer.toString();
+		System.out.println("------------------");
+		System.out.println(cc);
+		System.out.println("------------------");
+	}
+}
